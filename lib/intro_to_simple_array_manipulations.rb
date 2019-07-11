d5d0cea2ac2ff85a1738f8c35fb2697fead62074
Belle P <bellepx3@gmail.com>
2019-07-11 04:48:44 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -29,6 +29,16 @@ end
 def using_insert(array, element)
   array.insert(element)
 end
+
+def using_uniq(array)
+  array.uniq()
+end
+
+def using_flatten(array)
+  array.flatten()
+end
+
+def using_delete
   
   
   
